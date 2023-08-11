a4fecd28297eb65961a35bb37f90a5b8969983c6
Anderson-Calderon <anderson.calderoncampos@gmail.com>
2023-08-11 12:48:33 -0500

    solucionando UseAuth por useAuth

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import useAuth from '../hooks/UseAuth';
+import useAuth from '../hooks/useAuth';
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
 import {alertaEditarAgregarError} from '../helpers/Alerta';
