122e401bfd8fe72a8d5c5c9837cc1760715be5ec
Anderson-Calderon <anderson.calderoncampos@gmail.com>
2023-08-11 14:21:31 -0500

    resolviendo error

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -60,7 +60,7 @@ const Sidebar = ()=>{
 		        	to="/inicio">
 
 		          <i class="fa-solid fa-house"></i>
-		          <span>   Inicios</span>
+		          <span>   Inicios </span>
 
 		        </Link>
 
